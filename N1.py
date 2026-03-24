1ee6aef29f5e9b5155a4403b428165df6e2bcbe6
daniel603802 <danieldasilvarodrigues603@gmail.com>
2026-03-24 16:50:44 -0300

    Add files via upload

---
+++ N1.py
@@ -0,0 +1,8 @@
+n1 = int(input("primeira nota do aluno: "))
+n2 = int(input("segunda nota do aluno: "))
+n3 = int(input("terceira nota do aluno: "))
+n4 = int(input("quarta nota do aluno: "))
+
+m = (n1 + n2 + n3 + n4) / 4
+
+print('A média entre {}, {}, {}, {} é igual a {}'.format(n1, n2, n3, n4, m))
